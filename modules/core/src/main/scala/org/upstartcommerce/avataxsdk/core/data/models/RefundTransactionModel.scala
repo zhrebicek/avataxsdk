294011c2429bd87f7cedab3c214c823b48962db6
Yan Doroshenko <yan.doroshenko@upstartcommerce.com>
2019-06-07 09:22:42 +0200

    [upd] Update model

--- modules/core/src/main/scala/org/upstartcommerce/avataxsdk/core/data/models/RefundTransactionModel.scala
+++ modules/core/src/main/scala/org/upstartcommerce/avataxsdk/core/data/models/RefundTransactionModel.scala
@@ -14,16 +14,24 @@
  */
 
 package org.upstartcommerce.avataxsdk.core.data.models
-import java.sql.Date
+
+import java.time.Instant
+
 import org.upstartcommerce.avataxsdk.core.data.enums._
 
-final case class RefundTransactionModel(refundTransactionCode:Option[String] = None, refundDate:Date, refundType:Option[RefundType] = None, refundPercentage:Option[BigDecimal] = None, refundLines:Option[List[String]] = None, referenceCode:Option[String] = None) {
-  lazy val refundLinesRaw:List[String] = refundLines.getOrElse(List.empty)
-  def withRefundTransactionCode(value:String):RefundTransactionModel = copy(refundTransactionCode = Some(value))
-  def withRefundDate(value:Date):RefundTransactionModel = copy(refundDate = value)
-  def withRefundType(value:RefundType):RefundTransactionModel = copy(refundType = Some(value))
-  def withRefundPercentage(value:BigDecimal):RefundTransactionModel = copy(refundPercentage = Some(value))
-  def withRefundLines(value:List[String]):RefundTransactionModel = copy(refundLines = Some(value))
-  def withReferenceCode(value:String):RefundTransactionModel = copy(referenceCode = Some(value))
+final case class RefundTransactionModel(refundTransactionCode: Option[String] = None, refundDate: Instant, refundType: Option[RefundType] = None, refundPercentage: Option[BigDecimal] = None, refundLines: Option[List[String]] = None, referenceCode: Option[String] = None) {
+  lazy val refundLinesRaw: List[String] = refundLines.getOrElse(List.empty)
+
+  def withRefundTransactionCode(value: String): RefundTransactionModel = copy(refundTransactionCode = Some(value))
+
+  def withRefundDate(value: Instant): RefundTransactionModel = copy(refundDate = value)
+
+  def withRefundType(value: RefundType): RefundTransactionModel = copy(refundType = Some(value))
+
+  def withRefundPercentage(value: BigDecimal): RefundTransactionModel = copy(refundPercentage = Some(value))
+
+  def withRefundLines(value: List[String]): RefundTransactionModel = copy(refundLines = Some(value))
+
+  def withReferenceCode(value: String): RefundTransactionModel = copy(referenceCode = Some(value))
 }
   
